f3e9ae25a789e0988b6edff4ac1c86f6e05a1acf
Carlos Vázquez Losada <c.vazquezlosada@gmail.com>
2019-08-16 20:23:55 +0200

    Added CFG class.

---
+++ NNEvol/CFG/CFG.py
@@ -0,0 +1,36 @@
+class CFG(object):
+    def __init__(self):
+        self.prod = defaultdict(list)
+
+    def add_prod(self, lhs, rhs):
+        """ Add production to the grammar. 'rhs' can
+            be several productions separated by '|'.
+            Each production is a sequence of symbols
+            separated by whitespace.
+
+            Usage:
+                grammar.add_prod('NT', 'VP PP')
+                grammar.add_prod('Digit', '1|2|3|4')
+        """
+        prods = rhs.split('|')
+        for prod in prods:
+            self.prod[lhs].append(tuple(prod.split()))
+
+    def gen_random(self, symbol):
+        """ Generate a random sentence from the
+            grammar, starting with the given
+            symbol.
+        """
+        sentence = ''
+
+        # select one production of this symbol randomly
+        rand_prod = random.choice(self.prod[symbol])
+
+        for sym in rand_prod:
+            # for non-terminals, recurse
+            if sym in self.prod:
+                sentence += self.gen_random(sym)
+            else:
+                sentence += sym + ' '
+
+        return sentence
